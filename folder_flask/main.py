8a172f4eb1ae9454ae062494568615f5554eb4b6
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-10-16 11:04:30 +0900

    Update main.py

--- folder_flask/main.py
+++ folder_flask/main.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    output = "hello world"
+    return render_template('index.html', title='hello2', name=output)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8888, threaded=True)  
