d59546a6de0452f96a049b5ed1c42e96a4955e00
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2023-10-16 11:19:13 +0900

    Update main.py

--- folder_flask/main.py
+++ folder_flask/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     output = "hello world"
-    return render_template('index.html', title='hello2', name=output)
+    return render_template('index.html', title='hello2', output=output)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8888, threaded=True)  
